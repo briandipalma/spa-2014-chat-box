19e9f905042a6a9fa580491e3a121ae93a6af434
Brian Di Palma <offler@gmail.com>
2014-05-31 18:32:55 +0100

    Add ChatBoxActions.

---
+++ src/ChatBoxActions.js
@@ -0,0 +1,14 @@
+import ChatBoxConstants from './ChatBoxConstants';
+
+export default class {
+    constructor(chatBoxDispatcher) {
+        this.chatBoxDispatcher = chatBoxDispatcher;
+    }
+
+    sendMessage(messageText) {
+        this.chatBoxDispatcher.handleViewAction({
+            actionType: ChatBoxConstants.SEND_MESSAGE,
+            messageText: messageText
+        });
+    }
+};
