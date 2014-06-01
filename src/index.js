b8477f1719b89f3e81d7318637704aa10ad5b28b
Brian Di Palma <offler@gmail.com>
2014-06-01 18:24:01 +0100

    Export ChatBoxElement from package.
    Create a ChatBoxElement in the workbench.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-export function printHello() {
-	console.info("Hello");
+import {Dispatcher} from "flux-es6";
+
+import ChatBoxStore from "./ChatBoxStore";
+import ChatBoxActions from "./ChatBoxActions";
+
+export function createStoreAndActions() {
+    var chatBoxDispatcher = new Dispatcher();
+    var chatBoxStore = new ChatBoxStore();
+    var chatBoxActions = new ChatBoxActions(chatBoxDispatcher);
+
+    chatBoxDispatcher.register((payload) => chatBoxStore.handleDispatcherAction(payload));
+
+    return [chatBoxStore, chatBoxActions];
 }
+
+export {ChatBoxElement} from "./ChatBoxElement";
